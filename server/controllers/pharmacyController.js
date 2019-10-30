d5f4c64ef4a7fc3c3f060797ded4e3d8ae0cd576
EduardoContreras <eduardo.andre.contreras@gmail.com>
2019-10-30 20:30:29 -0300

    Was add transform param to uppercase

--- server/controllers/pharmacyController.js
+++ server/controllers/pharmacyController.js
@@ -1,6 +1,6 @@
 const boom = require('boom');
 const CustomError = require('../helpers/errors');
-const { isUndefined, isEmpty } = require('lodash');
+const { isUndefined, isEmpty, toUpper } = require('lodash');
 const { METROPOLITAN_REGION_ID } = require('../config/constants');
 const { findCommune, findPharmacies, getPharmaciesLocation } = require('../helpers/utils');
 const { getCommunesByRegionId } = require('../services/commune/communeRegion');
@@ -11,10 +11,10 @@ exports.getEnabledPharmacy = async (req, reply) => {
   try {
     const { communeName, pharmacyName } = req.query;
     const responseCommunes = await getCommunesByRegionId(METROPOLITAN_REGION_ID);
-    const foundCommune = findCommune(responseCommunes, communeName);
+    const foundCommune = findCommune(responseCommunes, toUpper(communeName));
     if (!isUndefined(foundCommune)) {
       const responsePharmacies = await getPharmaciesByRegionId(METROPOLITAN_REGION_ID);
-      const foundPharmacies = findPharmacies(responsePharmacies.data, pharmacyName, foundCommune.id);
+      const foundPharmacies = findPharmacies(responsePharmacies.data, toUpper(pharmacyName), foundCommune.id);
       if (!isUndefined(foundPharmacies) && !isEmpty(foundPharmacies)) {
         return getPharmaciesLocation(foundPharmacies);
       } else {
